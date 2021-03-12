dc7c4007682568498730fddafa8376d680d9d043
robMolloy <robmolloy@hotmail.co.uk>
2021-03-12 23:03:06 +0000

    camera and location support

--- quasar.conf.js
+++ quasar.conf.js
@@ -86,7 +86,7 @@ module.exports = function(/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ["Dialog"]
     },
 
     // animations: 'all', // --- includes all animations
--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,10 +1,40 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video class="full-width" autoplay ref="video" />
+      <video
+        class="full-width hihi"
+        autoplay
+        ref="video"
+        v-show="!imageCaptured"
+      />
+      <canvas
+        ref="canvas"
+        class="full-width"
+        height="200"
+        v-show="imageCaptured"
+      />
     </div>
     <div class="text-center q-pa-md">
-      <q-btn round color="grey-10" icon="eva-camera" size="lg" />
+      <q-btn
+        round
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        @click="captureImage"
+        v-if="hasCameraSupport"
+      />
+      <q-file
+        outlined
+        v-model="imageUpload"
+        accept="image/*"
+        label="Choose an image"
+        @input="captureImageFallback"
+        v-else
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
     </div>
     <div class="row justify-center q-ma-md">
       <q-input
@@ -18,11 +48,19 @@
       <q-input
         class="col col-sm-6"
         v-model="post.location"
+        :loading="locationLoading"
         label="Location"
         dense
       >
         <template v-slot:append>
-          <q-btn round dense flat icon="eva-navigation-2-outline" />
+          <q-btn
+            v-if="!locationLoading && locationSupported"
+            @click="getLocation"
+            round
+            dense
+            flat
+            icon="eva-navigation-2-outline"
+          />
         </template>
       </q-input>
     </div>
@@ -34,6 +72,7 @@
 
 <script>
 import { uid } from "quasar";
+import Axios from "axios";
 // require("md-gum-polyfill");
 
 export default {
@@ -46,18 +85,127 @@ export default {
         location: "",
         photo: null,
         date: Date.now()
-      }
+      },
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true,
+      locationLoading: false
     };
+  },
+  computed: {
+    locationSupported() {
+      const isSupported = "geolocation" in navigator;
+      return isSupported;
+    }
   },
   methods: {
     initCamera() {
       navigator.mediaDevices
         .getUserMedia({ video: true })
-        .then(stream => (this.$refs.video.srcObject = stream));
+        .then(stream => (this.$refs.video.srcObject = stream))
+        .catch(e => {
+          this.hasCameraSupport = false;
+        });
+    },
+    captureImage() {
+      let video = this.$refs.video;
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext("2d");
+
+      canvas.width = video.getBoundingClientRect().width;
+      canvas.height = video.getBoundingClientRect().height;
+
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.imageCaptured = true;
+
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+      this.disableCamera();
+    },
+    captureImageFallback(file) {
+      this.post.photo = file;
+
+      let canvas = this.$refs.canvas;
+      let context = canvas.getContext("2d");
+
+      let reader = new FileReader();
+      reader.onload = e => {
+        let img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          this.imageCaptured = true;
+        };
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop();
+      });
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI
+        .split(",")[0]
+        .split(":")[1]
+        .split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
+    },
+    getLocation() {
+      this.locationLoading = true;
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          this.$axios(
+            `https://geocode.xyz/${latitude},${longitude}?json=1`
+          ).then(result => {
+            this.locationSuccess(result);
+          });
+        },
+        err => this.locationError(),
+        { timeout: 7000 }
+      );
+    },
+    locationSuccess(result) {
+      let { city, country = "" } = result.data;
+      country = country ? ", " + country : "";
+
+      this.post.location = city + country;
+      this.locationLoading = false;
+    },
+    locationError(result) {
+      this.$q.dialog({
+        title: "Error",
+        message: "Could not find your location"
+      });
+      this.locationLoading = false;
     }
   },
   mounted() {
     this.initCamera();
+  },
+  beforeDestroy() {
+    if (this.hasCameraSupport) this.disableCamera();
   }
 };
 </script>
